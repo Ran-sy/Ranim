4fd34219eaed68d2262bd708bc3749645cd4158b
ran-sy <raneem.1999.ta@gmail.com>
2023-03-25 05:47:57 +0200

    add baseUrl

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+  compilerOptions: {
+    baseUrl: ".",
+  }
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
