128c3db8c822d189ef37c897eba8020881742360
ran-sy <raneem.1999.ta@gmail.com>
2023-03-25 08:10:42 +0200

    gh-pages

--- next.config.js
+++ next.config.js
@@ -1,8 +1,15 @@
 /** @type {import('next').NextConfig} */
+
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig = {
   reactStrictMode: true,
   resolve: {
     extensions: [".js", ".jsx"],
+  },
+  assetPrefix: isProd ? "ran-sy.github.io/Ranim" : "",
+  images: {
+    unoptimized: true,
   },
 };
 
