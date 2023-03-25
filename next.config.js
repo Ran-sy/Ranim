3d0d07b9098b3055c02cc7f155eeb9f915cef4fd
ran-sy <raneem.1999.ta@gmail.com>
2023-03-25 08:46:25 +0200

    deployment

--- next.config.js
+++ next.config.js
@@ -4,10 +4,8 @@ const isProd = process.env.NODE_ENV === "production";
 
 const nextConfig = {
   reactStrictMode: true,
-  resolve: {
-    extensions: [".js", ".jsx"],
-  },
-  assetPrefix: isProd ? "ran-sy.github.io/Ranim" : "",
+  distDir: "build",
+  assetPrefix: isProd ? "ran-sy.github.io/Ranim" : ".",
   images: {
     unoptimized: true,
   },
